c809b29b7b43ae4b3d86a3067641575f5fe8f01a
Denise <denisegelanga@gmail.com>
2023-08-28 12:26:16 +0800

    Merging resources

---
+++ WarriorWars/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+    class EntryPoint
+    {
+        static Random rnd = new Random();
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Pre", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Joel", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rnd.Next(0,10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ WarriorWars/Warrior.cs
@@ -0,0 +1,63 @@
+﻿using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 100;
+        private const int BAD_GUY_STARTING_HEALTH = 100;
+
+        private readonly Faction FACTION;
+
+        public bool IsAlive { get { return isAlive; }}
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        private Weapon weapon;
+        private Armor armor;
+
+        public Warrior(string name, Faction faction)
+        {
+            this.name = name;
+            this.FACTION = faction;
+
+            isAlive = true;
+
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+                    break; 
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = BAD_GUY_STARTING_HEALTH;
+                    break; 
+                default:
+                    break;
+            }
+            
+        }
+
+        public void Attack(Warrior enemy)
+        {
+            int damage = weapon.Damage / enemy.armor.ArmorPoints;
+            enemy.health -=  damage;
+
+            if(enemy.health <= 0) {
+                enemy.isAlive = false;
+                System.Console.WriteLine($"{enemy.name} is dead! {name} is victorious!");
+            }
+            else
+            {
+                System.Console.WriteLine($"{name} attacked {enemy.name} . {damage} was inflicted, remaining health is {health}.");
+            } 
+        }
+
+    }
+}
